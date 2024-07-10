9bd14bc0e49851d2cd59faa1bb3efbc5a8540d32
muriz-dev <mhmdrizki.dev@gmail.com>
2024-07-10 20:32:47 +0700

    initialize SMK Group Chat Finder project

---
+++ database/connection.php
@@ -0,0 +1,7 @@
+<?php 
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'smk_groupchat';
+
+    $connection = mysqli_connect($host, $username, $password, $database);

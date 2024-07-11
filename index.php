8143c534ba64f741004793a82d2892c0a6b1e062
muriz-dev <mhmdrizki.dev@gmail.com>
2024-07-11 14:00:27 +0700

    refactor: input readonly, ucwords name, footer copyright

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 session_start();
 
 if (!isset($_SESSION['login'])) {
-    header("Location: login.php");
-    exit;
+    header('Location: login.php');
+    exit();
 }
 
 ?>
@@ -22,7 +22,8 @@ if (!isset($_SESSION['login'])) {
 <body class="bg-light">
     <div class="container bg-success text-center p-5">
         <img src="assets/img/cn.png" alt="" width="auto" height="112px" class="mb-4">
-        <h3 class="text-white fw-normal">Selamat Datang <span class="fw-bold"><?= $_SESSION['nama'] ?></span></h3>
+        <h3 class="text-white fw-normal">Selamat Datang <span
+                class="fw-bold"><?= ucwords(strtolower($_SESSION['nama'])) ?></span></h3>
         <p class="text-white">Sebagai Peserta MPLS SMK CITRA NEGARA 2024</p>
     </div>
     <div class="container border-start border-end border-bottom bg-light p-4">
@@ -32,19 +33,19 @@ if (!isset($_SESSION['login'])) {
         </div>
         <div class="mb-3">
             <label class="form-label">Nama Siswa</label>
-            <input type="text" class="form-control" value="<?= $_SESSION['nama'] ?>">
+            <input type="text" class="form-control" value="<?= strtoupper($_SESSION['nama']) ?>" readonly>
         </div>
         <div class="mb-3">
             <label class="form-label">Jurusan</label>
-            <input type="text" class="form-control" value="<?= $_SESSION['jurusan'] ?>">
+            <input type="text" class="form-control" value="<?= strtoupper($_SESSION['jurusan']) ?>" readonly>
         </div>
         <div class="mb-3">
             <label class="form-label">Asal Sekolah</label>
-            <input type="text" class="form-control" value="<?= $_SESSION['asal_sekolah'] ?>">
+            <input type="text" class="form-control" value="<?= strtoupper($_SESSION['asal_sekolah']) ?>" readonly>
         </div>
         <div class="mb-3">
             <label class="form-label">Tanggal Daftar</label>
-            <input type="text" class="form-control" value="<?= $_SESSION['tanggal_daftar'] ?>">
+            <input type="text" class="form-control" value="<?= $_SESSION['tanggal_daftar'] ?>" readonly>
         </div>
         <a href="<?= $_SESSION['link'] ?>">
             <button class="btn fw-medium btn-success w-100 mt-3">Gabung Obrolan Grup</button>
@@ -52,6 +53,9 @@ if (!isset($_SESSION['login'])) {
         <a href="controllers/LogoutController.php">
             <button class="btn fw-medium btn-danger w-100 mt-3">Keluar</button>
         </a>
+        <footer class="text-center mt-5">
+            <p class="fs-6">© 2024 SMK Citra Negara. All Rights Reserved.</p>
+        </footer>
     </div>
 </body>
 

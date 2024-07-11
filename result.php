d0844fe3247fcfc4702c162205cca0fef45ec3f0
muriz-dev <mhmdrizki.dev@gmail.com>
2024-07-11 09:51:23 +0700

    add: set up index and result page

---
+++ result.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMK Group Chat Finder</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="bg-light">
+    <div class="container bg-success text-center p-5">
+        <h3 class="text-white fw-normal">Selamat Datang <span class="fw-bold">Muhamad Rizki</span></h3>
+        <p class="text-white">Sebagai Peserta MPLS SMK CITRA NEGARA 2024</p>
+    </div>
+    <div class="container border-start border-end border-bottom bg-light p-4">
+        <div class="mb-3">
+            <label class="form-label">Nomor Pendaftaran</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Nama Siswa</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Jurusan</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Asal Sekolah</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Tanggal Daftar</label>
+            <input type="text" class="form-control">
+        </div>
+        <button class="btn fw-medium btn-success w-100 mt-3">Gabung Obrolan Grup</button>
+    </div>
+</body>
+
+</html>
